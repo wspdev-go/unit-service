81dba244dfcde87408858433591f60187207e16b
andriy <andriy@horisen.com>
2026-04-12 16:28:42 +0200

    usrv transaction

--- internal/store/store.go
+++ internal/store/store.go
@@ -3,13 +3,16 @@ package store
 import "unit-service/internal/config"
 
 type Store interface {
-	GetReference()
-	GetQueue()
-	GetTransaction()
+	GetReference() Reference
+	GetQueue() Queue
+	GetTransaction() Transaction
 }
 
 type store struct {
-	cfg *config.Config
+	cfg         *config.Config
+	Reference   Reference
+	Queue       Queue
+	Transaction Transaction
 }
 
 func NewStore(cfg *config.Config) Store {
@@ -18,8 +21,32 @@ func NewStore(cfg *config.Config) Store {
 	}
 }
 
-func (s store) GetReference() {}
+func (s store) GetReference() Reference {
+	if s.Reference != nil {
+		return s.Reference
+	}
 
-func (s store) GetQueue() {}
+	s.Reference = NewReference(s.cfg)
 
-func (s store) GetTransaction() {}
+	return s.Reference
+}
+
+func (s store) GetQueue() Queue {
+	if s.Queue != nil {
+		return s.Queue
+	}
+
+	s.Queue = NewQueue(s.cfg)
+
+	return s.Queue
+}
+
+func (s store) GetTransaction() Transaction {
+	if s.Transaction != nil {
+		s.Transaction = nil
+	}
+
+	s.Transaction = NewTransaction(s.cfg)
+
+	return s.Transaction
+}
--- internal/store/transaction_store.go
+++ internal/store/transaction_store.go
@@ -1,1 +1,26 @@
 package store
+
+import "unit-service/internal/config"
+
+type Transaction interface {
+	IsOpen() bool
+	Close() error
+}
+
+type transaction struct {
+	cfg *config.Config
+}
+
+func NewTransaction(cfg *config.Config) Transaction {
+	return transaction{
+		cfg: cfg,
+	}
+}
+
+func (t transaction) IsOpen() bool {
+	return t.cfg.TransactionDB != nil
+}
+
+func (t transaction) Close() error {
+	return nil
+}
