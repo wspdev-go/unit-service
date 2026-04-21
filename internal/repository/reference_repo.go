d19062e34c373018533768565c53576f3f69ebc3
andriy <andriy@horisen.com>
2026-04-21 22:57:18 +0200

    refrepo

--- internal/repository/reference_repo.go
+++ internal/repository/reference_repo.go
@@ -24,7 +24,13 @@ type referenceRepo struct {
 func (r *referenceRepo) GetSctpConnList() ([]dao.SctpConn, error) {
 	sctpConnList := make([]dao.SctpConn, 0)
 
-	if err := r.store.DB().Find(&sctpConnList).Where("is_enable = 1").Error; err != nil {
+	db := r.store.DB()
+
+	if db == nil {
+		return sctpConnList, nil
+	}
+
+	if err := db.Where("is_enable = 1").Find(&sctpConnList).Error; err != nil {
 		return nil, err
 	}
 
@@ -34,7 +40,13 @@ func (r *referenceRepo) GetSctpConnList() ([]dao.SctpConn, error) {
 func (r *referenceRepo) GetM3uaAsConnList() ([]dao.M3UaAsConn, error) {
 	m3uaAsConnList := make([]dao.M3UaAsConn, 0)
 
-	if err := r.store.DB().Table("m3ua_as_conns").Find(&m3uaAsConnList).Error; err != nil {
+	db := r.store.DB()
+
+	if db == nil {
+		return m3uaAsConnList, nil
+	}
+
+	if err := db.Table("m3ua_as_conns").Where("is_enable = 1").Find(&m3uaAsConnList).Error; err != nil {
 		return nil, err
 	}
 
@@ -43,7 +55,12 @@ func (r *referenceRepo) GetM3uaAsConnList() ([]dao.M3UaAsConn, error) {
 
 func (r *referenceRepo) GetM3uaAspLinkList() ([]dao.M3UaAspLink, error) {
 	m3uaAspLinkList := make([]dao.M3UaAspLink, 0)
-	if err := r.store.DB().Table("m3ua_asp_links").Find(&m3uaAspLinkList).Error; err != nil {
+
+	db := r.store.DB()
+	if db == nil {
+		return m3uaAspLinkList, nil
+	}
+	if err := db.Table("m3ua_asp_links").Where("is_enable = 1").Find(&m3uaAspLinkList).Error; err != nil {
 		return nil, err
 	}
 
